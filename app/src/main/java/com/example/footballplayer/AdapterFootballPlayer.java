8e71d9c02ca1671dad1a2c8f8ef20156ce46825b
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2023-03-13 09:08:45 +0700

    Update AdapterFootballPlayer.java

--- app/src/main/java/com/example/footballplayer/AdapterFootballPlayer.java
+++ app/src/main/java/com/example/footballplayer/AdapterFootballPlayer.java
@@ -1,5 +1,50 @@
 package com.example.footballplayer;
 
-public class AdapterFootballPlayer
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class AdapterFootballPlayer extends RecyclerView.Adapter<AdapterFootballPlayer.ViewHolderPlayer>
 {
+    private Context ctx;
+    private ArrayList arrName, arrNumber, arrClub;
+    public AdapterFootballPlayer(Context ctx, ArrayList arrName, ArrayList arrNumber, ArrayList arrClub)
+    {
+        this.ctx = ctx;
+        this.arrName = arrName;
+        this.arrNumber = arrNumber;
+        this.arrClub = arrClub;
+    }
+    @NonNull
+    @Override
+    public ViewHolderPlayer onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(ctx).inflate(R.layout.list_item_player, parent, false);
+        return new ViewHolderPlayer(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderPlayer holder, int position)
+    {
+    }
+    @Override
+    public int getItemCount()
+    {
+        return 0;
+    }
+    public class ViewHolderPlayer extends RecyclerView.ViewHolder
+    {
+        private TextView tvName, tvNumber, tvClub;
+        public ViewHolderPlayer(@NonNull View itemView)
+        {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tv_name);
+            tvNumber = itemView.findViewById(R.id.tv_number);
+            tvClub = itemView.findViewById(R.id.tv_club);
+        }
+    }
 }
