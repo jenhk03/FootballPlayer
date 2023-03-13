8d3e02ae304d8cf7b0a9bf3b3f7870f5fee17d6e
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2023-03-13 09:33:47 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/footballplayer/MainActivity.java
+++ app/src/main/java/com/example/footballplayer/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.footballplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
-    MyDatabaseHelper myDB;
+    private MyDatabaseHelper myDB;
     private FloatingActionButton fabAdd;
+    private RecyclerView rvPlayer;
+    private AdapterFootballPlayer adapterFootballPlayer;
+    private ArrayList<String> arrName, arrNumber, arrClub;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvPlayer = findViewById(R.id.rv_player);
         myDB = new MyDatabaseHelper(MainActivity.this);
         fabAdd = findViewById(R.id.fab_add);
         fabAdd.setOnClickListener(new View.OnClickListener()
@@ -26,5 +35,40 @@ public class MainActivity extends AppCompatActivity
             }
         }
         );
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        ShowPlayer();
+    }
+
+    private void SQLiteToArrayList()
+    {
+        Cursor cursor = myDB.readDataPlayer();
+        if (cursor.getCount() == 0)
+        {
+            Toast.makeText(this, "Tidak Ada Data", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            while (cursor.moveToNext())
+            {
+                arrName.add(cursor.getString(1));
+                arrNumber.add(cursor.getString(2));
+                arrClub.add(cursor.getString(3));
+            }
+        }
+    }
+    private void ShowPlayer()
+    {
+        arrName = new ArrayList<>();
+        arrNumber = new ArrayList<>();
+        arrClub = new ArrayList<>();
+        SQLiteToArrayList();
+        adapterFootballPlayer = new AdapterFootballPlayer(MainActivity.this, arrName, arrNumber, arrClub);
+        rvPlayer.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        rvPlayer.setAdapter(adapterFootballPlayer);
     }
 }
