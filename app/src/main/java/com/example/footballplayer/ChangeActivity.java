2e4880ac1a3cfaa3ec70b6fe63be3384db0dfd85
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2023-03-20 09:00:05 +0700

    Update ChangeActivity.java

--- app/src/main/java/com/example/footballplayer/ChangeActivity.java
+++ app/src/main/java/com/example/footballplayer/ChangeActivity.java
@@ -3,8 +3,10 @@ package com.example.footballplayer;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ChangeActivity extends AppCompatActivity
 {
@@ -25,5 +27,45 @@ public class ChangeActivity extends AppCompatActivity
         String name = intent.getStringExtra("varName");
         String number = intent.getStringExtra("varNumber");
         String club = intent.getStringExtra("varClub");
+        etName.setText(name);
+        etNumber.setText(number);
+        etClub.setText(club);
+        btnChange.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String getName, getNumber, getClub;
+                getName = etName.getText().toString();
+                getNumber = etNumber.getText().toString();
+                getClub = etClub.getText().toString();
+                if (getName.trim().equals(""))
+                {
+                    etName.setError("Nama Player tidak boleh kosong!!");
+                }
+                else if (getNumber.trim().equals(""))
+                {
+                    etNumber.setError("Nomor Punggung tidak boleh kosong!!");
+                }
+                else if (getClub.trim().equals(""))
+                {
+                    etClub.setError("Klub tidak boleh kosong!!");
+                }
+                else
+                {
+                    long exec = myDB.changePlayer(id, getName, getNumber, getClub);
+                    if (exec == -1)
+                    {
+                        Toast.makeText(ChangeActivity.this, "Gagal mengubah data", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(ChangeActivity.this, "Sukses mengubah data", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+            }
+        }
+        );
     }
 }
