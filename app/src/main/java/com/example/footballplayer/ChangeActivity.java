f599d71e19a96f634a43b0cbce268833ab6fe148
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2023-03-20 08:40:58 +0700

    Update ChangeActivity.java

--- app/src/main/java/com/example/footballplayer/ChangeActivity.java
+++ app/src/main/java/com/example/footballplayer/ChangeActivity.java
@@ -1,14 +1,29 @@
 package com.example.footballplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ChangeActivity extends AppCompatActivity
 {
+    private EditText etName, etNumber, etClub;
+    private Button btnChange;
+    MyDatabaseHelper myDB = new MyDatabaseHelper(ChangeActivity.this);
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change);
+        etName = findViewById(R.id.et_name);
+        etNumber = findViewById(R.id.et_number);
+        etClub = findViewById(R.id.et_club);
+        btnChange = findViewById(R.id.btn_change);
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("varID");
+        String name = intent.getStringExtra("varName");
+        String number = intent.getStringExtra("varNumber");
+        String club = intent.getStringExtra("varClub");
     }
 }
